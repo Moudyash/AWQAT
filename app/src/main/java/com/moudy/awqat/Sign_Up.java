177771e4de9b8c83e3bd771ce4d44ec6acf08c98
Moudyash <you@example.com>
2022-09-04 16:11:51 +0300

    Initial commit

--- app/src/main/java/com/moudy/awqat/Sign_Up.java
+++ app/src/main/java/com/moudy/awqat/Sign_Up.java
@@ -2,13 +2,23 @@ package com.moudy.awqat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Sign_Up extends AppCompatActivity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    btn= (Button) findViewById(R.id.signupbtn);
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(Sign_Up.this,Phone_Verification.class) );
+        }
+    });
     }
 }
